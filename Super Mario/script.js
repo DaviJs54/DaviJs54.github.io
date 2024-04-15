6aae4ad075b4305d06ada5539685c9233c895a81
DaviJs54 <143214552+DaviJs54@users.noreply.github.com>
2024-04-15 17:14:17 -0300

    Add files via upload

---
+++ Super Mario/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const mario = document.querySelector('.mario');
+
+    const jump = () => {
+    
+        mario.classList.add('jump');
+
+        setTimeout(() => {
+            mario.classList.remove('jump');
+        }, 500);
+    }
+
+    document.addEventListener('keydown', jump);
+});
