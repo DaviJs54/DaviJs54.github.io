f7ea017e5e48f0d5db762f4e2b214455f3fda6d8
DaviJs54 <143214552+DaviJs54@users.noreply.github.com>
2025-08-27 15:37:25 -0300

    Delete Site directory

--- Site/script.js
+++
@@ -1,6 +0,0 @@
-page2 = `<div class="topo">Try hard gym</div>
-    <div class="options">
-        <div id="option" class="treino">Build your workout</div>
-        <div id="option" class="dieta">Build your diet</div>
-        <div id="option" class="corpo">Class your body</div>
-    </div>`
